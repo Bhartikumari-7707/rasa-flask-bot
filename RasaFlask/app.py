d2915a111324df421cc9df2033296b4cf4176f38
Bharti Kumari <bhartikumari77789@gmail.com>
2025-06-17 16:35:04 +0530

    app.py

--- RasaFlask/app.py
+++ RasaFlask/app.py
@@ -4,7 +4,7 @@ import requests
 # RASA_API_URL =  "http://127.0.0.1:5005/webhooks/rest/webhook"
 import os
 
-RASA_API_URL = os.getenv("RASA_API_URL", " http://127.0.0.1:5005/webhooks/rest/webhook")
+RASA_API_URL = os.getenv("RASA_API_URL", "http://127.0.0.1:5005/webhooks/rest/webhook")
 
 app = Flask(__name__)
 
